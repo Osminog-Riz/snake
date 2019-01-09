be91ffdbc19f9b928781ade2965f68c2f719e591
Margarita <osminog_riz@mail.ru>
2019-01-09 17:01:29 +0300

    добавлены нули в правых углах поля; добавлен покадровый вывод (который не работает в windows!);

--- snake.cpp
+++ snake.cpp
@@ -16,37 +16,73 @@ int x, y, frux, fruy, sc;
 enum edir { STOP=0,LEFT,RIGHT,UP,DOWN};
 edir dir;
 
+void printTo(int x, int y, string text) {
+    printf("\033[%d;%dH%s\n", x, y, text.c_str());
+		printf("\033[1;1H");
+}
+
 void Setup()
 {
 	srand(time(NULL));
 	gameover = 0;
-	dir = STOP;//ururur
+	dir = STOP;
 	x = width / 2;
 	y = height / 2;
 	frux = rand() % width;
 	fruy = rand() % height;
-	sc= 0;
+	sc = 0;
 }
+
 void Draw()
 {
 	system("cls");
-	for (int i = 0; i <= width; i++)
-		cout << "0";
-	cout << endl;
+	string display = "";
+	for (int i = 0; i < width + 2; i++) {
+		// cout << "0";
+		display += "0";
+	}
+	// cout << endl;
+	display += "\n";
 	for(int i=0;i<=height;i++)
 	{
 		for (int j = 0;j<=width;j++)
 		{
-			if (j == 0 || j == width) cout << "0";
-			if (i == y && j == x) cout << "Z";
-			else if (i == fruy && j == frux) cout << "*";
-			else cout << " ";
-		}cout << endl;
+			if (j == 0 || j == width) {
+					display += "0";
+					// cout << "0";
+				}
+			if (i == y && j == x) {
+					display += "Z";
+			// cout << "Z";
+		}
+			else if (i == fruy && j == frux) {
+				// cout << "*";
+					display += "*";
+				}
+			else 	display += " ";
+			// cout << " ";
+		}
+			display += "\n";
+		// cout << endl;
 	}
-	for (int i = 0; i <= width; i++)
-		cout << "0"; cout << endl;
-	cout <<"Your score = "<< sc << endl;
+	for (int i = 0; i < width + 2; i++) {
+		// cout << "0";
+		display += "0";
+		// cout << endl;
+	}
+	display += "\nYour score = ";
+	display += sc;
+	// cout<<endl;
+	display += "\r\n";
+	cout << display;
+
+	// printf("gfdg");
+	// for (int i = 20; i > 0; i--) {
+	// 	printTo(i, i, "Z");
+	// 	Sleep(30);
+	// }
 }
+
 void Input()
 {
 	if (_kbhit()) {
@@ -61,8 +97,8 @@ void Input()
 
 	}
 }
-void Large()
-{
+
+void Large() {
 	switch (dir)
 	{
 
@@ -75,13 +111,16 @@ void Large()
 	case DOWN:y++;
 		break;
 	}
-	if (x < 0 || x>width || y<0 || y>height) gameover = true;
-	if (x == frux && y == fruy)
-	{ sc++;
-	frux = rand() % width;
-	fruy = rand() % height;
+	if (x < 0 || x>width || y<0 || y>height)
+		gameover = true;
+
+	if (x == frux && y == fruy)	{
+		sc++;
+		frux = rand() % width;
+		fruy = rand() % height;
 	}
 }
+
 int main()
 {
 	setlocale(0, "rus");
@@ -93,7 +132,7 @@ int main()
 		Large();
     // usleep(50000);
     // std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    Sleep(50);
+    Sleep(30);
 	}
 	return 0;
 }
